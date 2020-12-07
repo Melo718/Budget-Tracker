e15b6c17e70d8543425fdae66921ac0bf971f3cb
camilo alvarez <camiloalvarez88@gmail.com>
2020-12-07 08:26:53 -0500

    updating server

--- server.js
+++ server.js
@@ -15,15 +15,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/BudgetTracker',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
 
 // routes
 app.use(require("./routes/api.js"));
